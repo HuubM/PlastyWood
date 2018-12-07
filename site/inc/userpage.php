f8bba2dbe2d8a4bdd3e69c3412db7c3fb17b63c4
Fatih Kerim Aktas <15129438@student.hhs.nl>
2018-12-07 22:56:38 +0100

    updated upload front-end

--- site/inc/userpage.php
+++ site/inc/userpage.php
@@ -1,63 +1,56 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-  <title>Bootstrap table</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Bootstrap table</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div class="container">
 
+    <h2>Welcome <?php print $_SESSION['user']['fname'] . ' ' . $_SESSION['user']['lname']; ?></h2>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php if ($_SESSION['user']['user_role'] == 2) {
+            foreach ($vars as $user) {
+                ?>
+                <tr>
+                    <td><?= $user['fname'] ?></td>
+                    <td><?= $user['lname'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td>
+                        <div>
+                            <form enctype="multipart/form-data" action="upload.php" method="POST">
+                                <input type="hidden" name="MAX_FILE_SIZE" value="100000"/>
+                                <input name="uploaded" type="file" style="display:inline"/>
 
-    <div class="body_padded">
-        <h1>Vulnerability: File Upload</h1>
-
-        <div class="vulnerable_code_area">
-            <form enctype="multipart/form-data" action="upload.php" method="POST">
-                <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-                <p>Choose an image to upload:</p>
-                <input name="uploaded" type="file" /><br /><br />
-
-                <input type="submit" name="Upload" value="Upload" />
-            </form>
-        </div>
-    </div>
-
-
-
-
-    <h2>Welcome <?php print $_SESSION['user']['fname'].' '.$_SESSION['user']['lname']; ?></h2>
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead><tbody>
-    <?php if($_SESSION['user']['user_role'] == 2){
-    	foreach ($vars as $user) {
-    	?>
-    		<tr>
-		        <td><?=$user['fname']?></td>
-		        <td><?=$user['lname']?></td>
-		        <td><?=$user['email']?></td>
-		    </tr>
-    	<?php
-    	}
-    }else{ ?>
-      <tr>
-        <td><?=$_SESSION['user']['fname']?></td>
-        <td><?=$_SESSION['user']['lname']?></td>
-        <td><?=$_SESSION['user']['email']?></td>
-      </tr>    
-    <?php } ?></tbody>
-  </table>
-  <p><a href='logout.php'>Logout</a></p>
+                                <input type="submit" name="Upload" value="Upload" style="display:inline"/>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                <?php
+            }
+        } else { ?>
+            <tr>
+                <td><?= $_SESSION['user']['fname'] ?></td>
+                <td><?= $_SESSION['user']['lname'] ?></td>
+                <td><?= $_SESSION['user']['email'] ?></td>
+            </tr>
+        <?php } ?></tbody>
+    </table>
+    <p><a href='logout.php'>Logout</a></p>
 </div>
 
 </body>
